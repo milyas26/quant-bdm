420a104448f2493c46c959c9e5744f738bba99cc
milyas-jubelio <muhammad.ilyas@jubelio.com>
2026-03-07 05:45:34 +0700

    feat: init

---
+++ src/components/app-sidebar.tsx
@@ -0,0 +1,62 @@
+import * as React from "react"
+
+import {
+  Sidebar,
+  SidebarContent,
+  SidebarGroup,
+  SidebarGroupContent,
+  SidebarGroupLabel,
+  SidebarHeader,
+  SidebarMenu,
+  SidebarMenuButton,
+  SidebarMenuItem,
+  SidebarRail,
+} from "@/components/ui/sidebar"
+
+// This is sample data.
+const data = {
+  navMain: [
+    {
+      title: "Menu",
+      url: "#",
+      items: [
+        { title: "Dashboard", url: "#", isActive: true },
+        { title: "Market Scanner", url: "#" },
+        { title: "Smart Watchlist", url: "#" },
+        { title: "Sector Flow", url: "#" },
+        { title: "Stock Detail", url: "#" },
+        { title: "Broker Analysis", url: "#" },
+        { title: "Accumulation Detector", url: "#" },
+      ],
+    },
+  ],
+}
+
+export function AppSidebar({ ...props }: React.ComponentProps<typeof Sidebar>) {
+  return (
+    <Sidebar {...props}>
+      <SidebarHeader>
+        <div className="font-semibold text-2xl">STOCKPICKER</div>
+      </SidebarHeader>
+      <SidebarContent>
+        {data.navMain.map((item) => (
+          <SidebarGroup key={item.title}>
+            <SidebarGroupLabel>{item.title}</SidebarGroupLabel>
+            <SidebarGroupContent>
+              <SidebarMenu>
+                {item.items.map((item) => (
+                  <SidebarMenuItem key={item.title}>
+                    <SidebarMenuButton asChild isActive={item.isActive}>
+                      <a href={item.url}>{item.title}</a>
+                    </SidebarMenuButton>
+                  </SidebarMenuItem>
+                ))}
+              </SidebarMenu>
+            </SidebarGroupContent>
+          </SidebarGroup>
+        ))}
+      </SidebarContent>
+      <SidebarRail />
+    </Sidebar>
+  )
+}
